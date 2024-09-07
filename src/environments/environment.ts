a86f5a26bd5d7b1aa8f456b60838bcdc2772a897
rajanhere <rajan@brainstackhub.com.au>
2024-09-07 10:54:31 +1000

    added gitIgnore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: false,
 
-firebase : {
-  apiKey: 'AIzaSyDnc5WTuSGdixITF7YJcY3pEaCcJCHjiQw',
-  authDomain: 'car-management-bfe89.firebaseapp.com',
-  projectId: 'car-management-bfe89',
-  storageBucket: 'car-management-bfe89.appspot.com',
-  messagingSenderId: '487978124415',
-  appId: '1:487978124415:web:c13d37580f8078e10e6b4a',
-}
+  firebase: {
+    apiKey: 'AIzaSyDnc5WTuSGdixITF7YJcY3pEaCcJCHjiQw',
+    authDomain: 'car-management-bfe89.firebaseapp.com',
+    projectId: 'car-management-bfe89',
+    storageBucket: 'car-management-bfe89.appspot.com',
+    messagingSenderId: '487978124415',
+    appId: '1:487978124415:web:83673c8b83cec6090e6b4a'
+  },
 };
 
 
